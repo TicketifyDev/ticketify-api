8150cc506b5be05560c5e1f9f07b468ed364cfaa
YashwanthSimhaa <yashwanthsimha9258@gmail.com>
2024-06-29 00:02:39 +0530

    added code for generating access token

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,12 +1,37 @@
 from passlib.context import CryptContext
+import traceback
 
 # Initialize CryptContext for hashing passwords
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# Function to hash the password
+
 def hash_password(password: str):
+    """
+    Function to hash the password
+    """
     return pwd_context.hash(password)
 
-# Function to verify a password
+
 def verify_password(plain_password: str, hashed_password: str):
+    """
+    Function to verify the password
+    """
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(data : dict, username: str, password: str):
+    """
+    Function to Authenticate a user by verifying their `username` and `password`.
+    """
+    try:
+        if username in data:
+            user_dict = data[username]
+            hashed_password = user_dict["password"]
+            if not verify_password(password, hashed_password):
+                return False
+            return user_dict
+        
+    except Exception as e :
+        exception_details = traceback.format_exc()
+        print(f"An error occurred due to '{e}' : {exception_details}")
+        return False
---
+++ src/endpoints/login.py
@@ -0,0 +1,69 @@
+from fastapi import APIRouter, HTTPException, status
+from fastapi.responses import JSONResponse
+from schemas.auth_schema import AuthModel
+from common.status_codes import status_codes
+from common.json_operations import read_json_data
+from common.utils import authenticate_user
+from auth.auth_token import create_access_token
+from pathlib import Path
+from datetime import timedelta
+import traceback
+
+router = APIRouter()
+
+@router.post('/organizer-login',
+             tags=["Organizer Management"],
+             status_code=200,
+             responses={
+                200 : status_codes["response_200"],
+                401 : status_codes["response_401"],
+                500 : status_codes["response_500"]
+             })
+async def login_as_organizer(details : AuthModel):
+
+    """ 
+    API for authenticating organizers and generating access tokens by validating their `username` and `password`.
+    """
+
+    try :
+        # Navigate to the directory where json file with organizer details exists
+        current_directory= Path(__file__).parents[1]
+        response_file="organizer_details.json"
+        filename = current_directory / 'responses' / response_file
+
+        # Get all organizers information
+        existing_data = read_json_data(filename)
+
+        # Authenticate Organizer details
+        organizer = authenticate_user(existing_data, details.username, details.password)
+        if not organizer:
+            return JSONResponse(
+                content={"message" : "Invalid credentials"},
+                status_code=status.HTTP_401_UNAUTHORIZED
+            )
+        
+        # Generate JWT Access Token
+        access_token_expires = timedelta(minutes=30)
+        access_token = create_access_token(
+            data = {"sub": details.username}, 
+            expires_delta = access_token_expires
+            )
+        
+        return JSONResponse(
+            content = {
+                "message": "Login successful",
+                "access_token": access_token,
+                "token_type": "bearer",
+                "expires_in": 1800
+            },
+            status_code=status.HTTP_200_OK
+        )
+
+    except Exception as e :
+        exception_details = traceback.format_exc()
+        print(f"An error occurred due to '{e}' : {exception_details}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An unexpected error occurred"
+        )
+
