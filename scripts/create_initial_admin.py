dd933a6bb7c6cbb1d62cfd403a944515c73d19cd
YashwanthSimhaa <yashwanthsimha9258@gmail.com>
2024-07-15 20:57:22 +0530

    placed scripts directory outside src

--- scripts/create_initial_admin.py
+++ scripts/create_initial_admin.py
@@ -1,6 +1,7 @@
 import sys
 import getpass 
 import re
+import time
 from pathlib import Path
 
 # Determine the parent directory of the current script's file path.
@@ -9,9 +10,9 @@ parent_directory_resolved = Path(__file__).resolve().parents[1]
 # Add the parent directory to the sys.path list to allow importing modules from that directory.
 sys.path.append(str(parent_directory_resolved))
 
-from common.json_operations import read_json_data, create_json_response
-from common.utils import hash_password
-from schemas.registration_schema import NAME_REGEX, USERNAME_REGEX, PASSWORD_REGEX, EMAIL_REGEX
+from src.common.json_operations import read_json_data, create_json_response
+from src.common.utils import hash_password
+from src.schemas.registration_schema import NAME_REGEX, USERNAME_REGEX, PASSWORD_REGEX, EMAIL_REGEX
 
 parent_directory = Path(__file__).parents[1]
 admin_file = "initial_admin.json"
@@ -22,7 +23,7 @@ def get_valid_input(prompt: str, regex: str, error_message: str):
     Function to Prompt the user for input and validate it against a regex pattern. \n
     Repeats the prompt until the input is valid.
 
-    Args:
+    Parameters:
         `prompt` : The input prompt message
         `regex` : The regex pattern to validate the input
         `error_message` : The error message to display for invalid input
@@ -48,8 +49,10 @@ def create_initial_admin():
         return 
     
     # Continue to create new admin by prompting user to input admin details if file doesn't exist or is empty
+    print("\nCreating Initial Admin account...")
+    time.sleep(1)
     name = get_valid_input(
-        "\n Enter the admin full name : ",
+        "\n Enter admin full name : ",
         NAME_REGEX,
         "Invalid name. Name can only contain letters, spaces, apostrophes, and hyphens."
     )
