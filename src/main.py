d3f4bbccf432b364f124b4deac07192994c9a98a
YashwanthSimhaa <yashwanthsimha9258@gmail.com>
2024-07-17 20:08:47 +0530

    renamed file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from endpoints import login
+from endpoints import get_profile, login
 from common.descriptions import tags_metadata
-from endpoints import register, event_management, health_check, profile
+from endpoints import register, event_management, health_check
 
 app = FastAPI(title="Ticketify-API", openapi_tags=tags_metadata)
 
@@ -9,4 +9,4 @@ app.include_router(register.router)
 app.include_router(login.router)
 app.include_router(event_management.router)
 app.include_router(health_check.router)
-app.include_router(profile.router)
+app.include_router(get_profile.router)
