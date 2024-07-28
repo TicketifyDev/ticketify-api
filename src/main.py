6640d60f1d080fcaa30ad83f815ba26b2b9f245c
YashwanthSimhaa <yashwanthsimha9258@gmail.com>
2024-07-28 21:43:30 +0530

    added code for T-10/pending-organizer-requests

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from endpoints import get_profile, login
 from common.descriptions import tags_metadata
 from endpoints import register, event_management, health_check
+from endpoints.admin_management import pending_organizer_requests
 
 app = FastAPI(title="Ticketify-API", openapi_tags=tags_metadata)
 
@@ -10,3 +11,4 @@ app.include_router(login.router)
 app.include_router(event_management.router)
 app.include_router(health_check.router)
 app.include_router(get_profile.router)
+app.include_router(pending_organizer_requests.router)
