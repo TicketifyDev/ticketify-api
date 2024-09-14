2d06ec728b2e79dcb7f3aec3cb7534ad01624015
YashwanthSimhaa <yashwanthsimha9258@gmail.com>
2024-09-14 13:34:09 +0530

    separated router and logics

--- src/common/utils.py
+++ src/common/utils.py
@@ -40,6 +40,27 @@ def authenticate_user(data : dict, username: str, password: str):
         print(f"An error occurred due to '{e}' : {exception_details}")
         return False
     
+
+def handle_internal_server_error(exc: Exception):
+    """
+    Function to handle internal server errors and format the response.
+
+    Args:
+        exc (Exception): The caught exception.
+
+    Returns:
+        HTTPException: An HTTPException with a 500 status code and formatted response.
+    """
+    exception_details = traceback.format_exc()
+    print(f"An error occurred due to '{exc}' : {exception_details}")
+    raise HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail={
+            "status_code" : 500,
+            "message" : "An unexpected error occurred. Please try again later."
+        }
+    )
+
     
 def response_content(
         status_code : int, 
--- src/router.py
+++ src/router.py
@@ -1,21 +1,15 @@
 from fastapi import APIRouter
 from src.endpoints import (
-    register,
-    login,
+    user,
+    organizer,
     event_management,
-    health_check,
-    get_profile,
-    get_organizer_status,
-    update_profile
+    health_check
 )
 
 router = APIRouter()
 
-router.include_router(register.router)
-router.include_router(login.router)
-router.include_router(event_management.router)
 router.include_router(health_check.router)
-router.include_router(get_organizer_status.router)
-router.include_router(get_profile.router)
-router.include_router(update_profile.router)
+router.include_router(user.router)
+router.include_router(organizer.router)
+router.include_router(event_management.router)
 
