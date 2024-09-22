b54850d411b88c54ef3a2f1bd47f8ac436539bb6
VarshaNMurthy <varsha.narasimhamurthy@estuate.com>
2024-09-22 15:13:01 +0530

    get user details api

--- src/endpoints/user.py
+++ src/endpoints/user.py
@@ -1,12 +1,15 @@
-from fastapi import APIRouter, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, HTTPException, Security
+from src.schemas.auth_schema import AuthModel
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from src.schemas.registration_schema import user_registration
 from src.common.status_codes import status_codes
 from src.common.utils import handle_internal_server_error
 from src.endpoints.user_management.user_register import user_register
+from src.endpoints.user_management.get_user_profile import get_user_profile
 
 router = APIRouter(tags=["User Management"])
-
+token = HTTPBearer()
+ 
 @router.post('/user-register',
             status_code=201,
             responses={
@@ -30,3 +33,26 @@ async def new_user_registration(details : user_registration):
     except Exception as exc:
         handle_internal_server_error(exc)
 
+
+@router.get('/user-profile',
+           status_code=200,
+           responses={
+               400 : status_codes["response_400"],
+               401 : status_codes["response_401"],
+               403 : status_codes["response_401"],
+               404 : status_codes["response_404"],
+               500 : status_codes["response_500"]
+           })
+async def Fetch_user_profile(credentials : HTTPAuthorizationCredentials = Security(token)):
+    """
+    API for retrieving logged-in user's profile information.
+    """
+    try :
+        response = await get_user_profile(credentials)
+        return response
+    
+    except HTTPException as http_exc:
+        raise http_exc
+
+    except Exception as exc :
+        handle_internal_server_error(exc)
