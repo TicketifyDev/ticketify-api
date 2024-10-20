6d27b736de2de70c6038195dea3f1ea0b4b8643c
Dattatreya1234 <dattatreyapatil74@gmail.com>
2024-10-20 21:09:07 +0530

    added back the pending organizer request api

--- src/endpoints/admin.py
+++ src/endpoints/admin.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, HTTPException
-from fastapi.security import HTTPBearer
+from fastapi import APIRouter, HTTPException, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from src.schemas.auth_schema import AuthModel
 from src.common.status_codes import status_codes
 from src.common.utils import handle_internal_server_error
+from src.endpoints.admin_management.pending_organizer_requests import pending_organizer_registration_requests
 from src.common.constants import ADMINS_COLLECTION
+from src.common.constants import ORGANIZERS_COLLECTION
 from src.common.db import MongoDB
 from src.endpoints.admin_management.admin_login import admin_login
 
@@ -12,7 +14,32 @@ token = HTTPBearer()
 
 # Create an instance of MongoDB class by providing a collection name
 collection = MongoDB(ADMINS_COLLECTION)
+organizers_collection = MongoDB(ORGANIZERS_COLLECTION)
+
+@router.get('/pending-organizer-requests',
+            status_code = 200,
+            responses={
+                400 : status_codes["response_400"],
+                401 : status_codes["response_401"],
+                403 : status_codes["response_401"],
+                404 : status_codes["response_404"],
+                500 : status_codes["response_500"]
+            }
+        )
+async def get_pending_organizer_registration_requests(credentials : HTTPAuthorizationCredentials = Security(token)):
+    """
+    API for administrators to view all registration requests submitted by Organizers that are `under_review`.
+    """
+    try:
+        response = await pending_organizer_registration_requests(credentials, organizers_collection)
+        return response
     
+    except HTTPException as http_exc:
+        raise http_exc
+
+    except Exception as exc :
+        handle_internal_server_error(exc)
+
 
 @router.post('/admin-login',
              status_code=200,
