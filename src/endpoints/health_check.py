a3fd696da320b7cb5ac547f9eab2b8537c25f735
VarshaNarasimhamurthy <varsha.narasimhamurthy@estuate.com>
2025-01-19 13:06:20 +0530

    fixed vulnerability issue and added code for health check api

--- src/endpoints/health_check.py
+++ src/endpoints/health_check.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import status, APIRouter
+from src.common.utils import response_content
 from fastapi.responses import JSONResponse
 
 router = APIRouter(prefix="/api/v1", tags=["Health Check"])
@@ -8,4 +9,10 @@ async def health_check():
     """
     API to check whether the web server is up or down.
     """
-    return JSONResponse(content={"status":"Server is up and running"},status_code=200)
+    return JSONResponse(
+        content=response_content(
+            200,
+            "Server is up and running",  
+        ),
+        status_code=status.HTTP_200_OK
+    )
