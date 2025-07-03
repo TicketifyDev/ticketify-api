cd9fc56525921dac19f7ca705368786c2fa6f895
Dattatreya1234 <dattatreyapatil74@gmail.com>
2025-07-04 00:06:33 +0530

    implemented events review api

--- src/endpoints/admin.py
+++ src/endpoints/admin.py
@@ -6,9 +6,11 @@ from src.common.utils import handle_internal_server_error
 from src.endpoints.admin_management.admin_login import admin_login
 from src.endpoints.admin_management.get_admin_profile import get_admin_profile
 from src.endpoints.admin_management.organizer_requests import organizer_registration_requests
+from src.endpoints.admin_management.review_event_request import review_event_registration_request
 from src.endpoints.admin_management.review_organizer_request import review_organizer_registration_request
 from src.schemas.admin_management_schema import RegistrationStatus, ReviewRequest
 from src.common.constants import ADMINS_COLLECTION
+from src.common.constants import EVENTS_COLLECTION
 from src.common.constants import ORGANIZERS_COLLECTION
 from src.common.db import MongoDB, MongoDBCollectionProvider
 from src.common.logging_config import logger
@@ -18,6 +20,7 @@ token = HTTPBearer()
 
 # Create an instance of MongoDB class by providing a collection name
 collection = MongoDB(ADMINS_COLLECTION)
+events_collection = MongoDB(EVENTS_COLLECTION)
 organizers_collection = MongoDB(ORGANIZERS_COLLECTION)
 
 
@@ -177,3 +180,41 @@ async def review_organizer_registration(
 async def get_event_registration_requests():
     # implementation goes here
     pass
+
+
+@router.patch('/events-review/{title}',
+              status_code = 200,
+              responses={
+                400 : status_codes["response_400"],
+                401 : status_codes["response_401"],
+                403 : status_codes["response_401"],
+                404 : status_codes["response_404"],
+                500 : status_codes["response_500"]
+            })
+async def review_event_registration(
+    title : str,
+    review : ReviewRequest,
+    credentials : HTTPAuthorizationCredentials = Security(token)
+):
+    """
+    API for administrators to review and approve/reject an event's registration request.
+    """
+    logger.info(f"GET '/admins/events-review/{title}' API is invoked.")
+    try :
+        logger.debug("Validating token and checking administrator privileges.")
+        response = await review_event_registration_request(
+            title,
+            review,
+            events_collection,
+            credentials
+        )
+        logger.info("Successfully reviewed event registration requests.")
+        return response
+
+    except HTTPException as http_exc:
+        raise http_exc
+    
+    except Exception as e :
+        logger.error(f"Unexpected error occurred in '/admins/events-review/{title}' : {e}")
+        handle_internal_server_error(e)
+
