208dc4e87abcfcf8ee28faa89038399d465a665b
VarshaNarasimhamurthy <varsha.narasimhamurthy@estuate.com>
2025-07-22 22:04:23 +0530

    venue manager login code

--- src/common/constants.py
+++ src/common/constants.py
@@ -11,3 +11,4 @@ VENUE_MANAGERS_COLLECTION = 'venue_managers'
 USER_LOGIN_COLLECTION = "users_login_history"
 ORGANIZER_LOGIN_COLLECTION = "organizers_login_history"
 ADMIN_LOGIN_COLLECTION = "admins_login_history"
+VENUE_MANAGER_LOGIN_COLLECTION = "venue_manager_login_history"
