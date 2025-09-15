16d819993fe0abf1f6f26c0c5e2ae5c56bd37c32
YashwanthSimhaa <yashwanthsimha9258@gmail.com>
2025-09-15 21:39:51 +0530

    changed PATCH /events API payload as per new POST /events API

--- src/schemas/event_management_schema.py
+++ src/schemas/event_management_schema.py
@@ -1,8 +1,6 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, Field
 from datetime import date, time
 from typing import Optional
-
-
 
 class venue_dates(BaseModel):
     date: date
@@ -46,11 +44,11 @@ class update_event(BaseModel):
     title : Optional[str] = Field(None,description = "Title of the event")
     release_date : Optional[date] = Field(None,description = "Release date of the event")
     duration : Optional[int] = Field( None, description = "Duration of the event")
-    languages : list[str] = Field(..., examples = [["English"],["Hindi"],["Kannada"]], description = "Languages of the event")
+    languages : list[str] = Field(None, examples = [["English"],["Hindi"],["Kannada"]], description = "Languages of the event")
     genre : Optional[list[str]] = Field(None, examples = [["Thriller"],["Comedy"]], description = "Genre of the event")
-    censor: str = Field(..., examples = ["UA13+"], description = "Censorship of the event")
-    dimension: list[str] = Field(..., examples = [["3D"],["2D"]], description = "Dimension of the event")
+    censor: str = Field(None, examples = ["UA13+"], description = "Censorship of the event")
+    dimension: list[str] = Field(None, examples = [["3D"],["2D"]], description = "Dimension of the event")
     cast : Optional[list[str]] = Field(None, description = "Cast involved in the event")
     crew : Optional[list[str]] = Field( None, description = "Crew involved in making of the event")
-    description : str = Field(..., description = "Description of the event")
+    description : str = Field(None, description = "Description of the event")
     venues : Optional[list[venue_list]] = Field(None)
